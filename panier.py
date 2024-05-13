19a569de11ed7d62728470ef681bd91c82975e85
Guindoservice <abdala.guindo@odc.local>
2024-05-13 14:49:19 +0000

    modification

--- panier.py
+++ panier.py
@@ -7,12 +7,14 @@ class Panier:
         self.produits.append({"nom": nom_produit, "prix": prix_produit})
     # fonction suppression
     def supprimer_produit(self, index):
-        if index >= 0 and index < len(self.produits):
-            del self.produits[index]
+        if self.verifie_panier():
+            if index >= 0 and index < len(self.produits):
+                del self.produits[index]
     # FOUNCTION MODIFICATION
     def modifier_produit(self, index, nom_produit, prix_produit):
-        if index >= 0 and index < len(self.produits):
-            self.produits[index] = {"nom": nom_produit, "prix": prix_produit}
+        if self.verifie_panier():
+            if index >= 0 and index < len(self.produits):
+                self.produits[index] = {"nom": nom_produit, "prix": prix_produit}
 
     #fonction total produits
     def total_produits(self):
@@ -20,34 +22,41 @@ class Panier:
         for produit in self.produits:
             total += produit["prix"]
         return total
+    # Vérification d'état du panier
+    def verifie_panier(self):
+        if not self.produits:
+            print("Le panier est vide")
+            return False
+        return True
 
 # Exemple d'utilisation
 panier = Panier()
-print("ajouter un prduit au panier")
-panier.ajouter_produit("Pc", 1000.08)
-panier.ajouter_produit("Téléphone", 13000.100)
-panier.ajouter_produit("SACK", 1000)
 
-#enummuration des produit dans panier
-print("Produits dans le panier :")
-for i, produit in enumerate(panier.produits):
-    print(f"{i+1}. {produit['nom']} - {produit['prix']}")
+while True:
+    choix= input("Que voulez vous faire ? '(1. ajouter/ 2. Modifier/ 3. supprimer/ 4. Afficher/ 5. quiter)")
+    if choix== "1":
+        nom_produit=input("Nom du produit")
+        prix_produit = float(input("Prix du prix"))
+        panier.ajouter_produit(nom_produit,prix_produit)
+        print(f"Le Produit {nom_produit}, Le prix : {prix_produit} a été ajouté au panier ")
+    elif choix == '2':
+        index_produit= int(input("Index du produit"))
+        nom_produit= input("Nouveau nom du prosuit")
+        prix_produit = float(input("Nouveau prix du produit"))
+        panier.modifier_produit(index_produit,prix_produit,prix_produit)
+        print(f"Le produit a l'index {index_produit}, a été modifier ")
+    elif choix == "3":
+        index_produit= int(input("index du produit à suppriner"))
+        panier.supprimer_produit(index_produit)
+        print(f"Le produit à l'index {index_produit} a été supprimer")
 
-# Afficher total des produits
-print(f"Total des produits : {panier.total_produits()} FCFA ")
+    elif choix == "4":
+        print('Produit dans le panier')
+        for i, produit in enumerate(panier.produits):
+            print(f"{i + 1}. {produit['nom']} - {produit['prix']}")
+        print(f"Total des produit : {panier.total_produits()}")
 
-panier.modifier_produit(1, "Souris", 2000)
-print("\nProduits modifiés dans le panier :")
-for i, produit in enumerate(panier.produits):
-    print(f"{i+1}. {produit['nom']} - {produit['prix']} FCFA ")
-
-# Afficher total des produits après modification
-print(f"Total des produits après modification : {panier.total_produits()} FCFA")
-
-panier.supprimer_produit(2)
-print("\nProduits restants dans le panier :")
-for i, produit in enumerate(panier.produits):
-    print(f"{i+1}. {produit['nom']} - {produit['prix']} FCFA")
-
-# Afficher total des produits  apres suppression
-print(f"Total des produits après suppression : {panier.total_produits()} FCFA")
+    elif choix == "5":
+        break
+    else:
+        print('Choix invalide veuillez réessayer.')
