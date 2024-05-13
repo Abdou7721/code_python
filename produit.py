6c9c84272a525c6b6bb942d879f6f0157a7bff52
unknown <mohamed.kone1@odc.local>
2024-05-13 10:08:18 +0000

    Class Produit

---
+++ produit.py
@@ -0,0 +1,11 @@
+class Produit:
+    quantiteProduit = 0
+    # liste_produits = []
+    def __init__(self,id_produit,libelle,date,prix):
+        self.id_produit = id_produit
+        self.libelle = libelle
+        self.date = date
+        self.prix = prix
+        # Produit.liste_produits.append(self)
+
+    
