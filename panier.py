e4254c520f5d638fd56adc7a6381abe4c297be2e
Guindoservice <abdala.guindo@odc.local>
2024-05-13 15:47:34 +0000

    modification

--- panier.py
+++ panier.py
@@ -33,30 +33,35 @@ class Panier:
 panier = Panier()
 
 while True:
+    # pour choix des options
     choix= input("Que voulez vous faire ? '(1. ajouter/ 2. Modifier/ 3. supprimer/ 4. Afficher/ 5. quiter)")
+    # pour ajouter les prduits dans le panier
     if choix== "1":
         nom_produit=input("Nom du produit")
         prix_produit = float(input("Prix du prix"))
         panier.ajouter_produit(nom_produit,prix_produit)
-        print(f"Le Produit {nom_produit}, Le prix : {prix_produit} a été ajouté au panier ")
+        print(f"Le Produit {nom_produit}, Le prix : {prix_produit}FCFA a été ajouté au panier ")
+        # pour modifier
     elif choix == '2':
         index_produit= int(input("Index du produit"))
         nom_produit= input("Nouveau nom du prosuit")
         prix_produit = float(input("Nouveau prix du produit"))
         panier.modifier_produit(index_produit,prix_produit,prix_produit)
         print(f"Le produit a l'index {index_produit}, a été modifier ")
+        # la condition pour supprimer
     elif choix == "3":
         index_produit= int(input("index du produit à suppriner"))
         panier.supprimer_produit(index_produit)
         print(f"Le produit à l'index {index_produit} a été supprimer")
-
+    # pour visualiser les produits qui sont dans le panier
     elif choix == "4":
         print('Produit dans le panier')
         for i, produit in enumerate(panier.produits):
             print(f"{i + 1}. {produit['nom']} - {produit['prix']}")
-        print(f"Total des produit : {panier.total_produits()}")
-
+        print(f"Total des produit : {panier.total_produits() }FCFA")
+    # pour sortir dans l'option
     elif choix == "5":
         break
+        # aus cas ou vous avez mal choisit l'option
     else:
         print('Choix invalide veuillez réessayer.')
