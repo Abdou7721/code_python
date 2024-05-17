c7294a221b1b8bdb9b8e639e9c2afbe57dc0d097
Harouna Diallo <70182073+Harouna-Ness@users.noreply.github.com>
2024-05-17 10:46:58 +0000

    Update commande.py

--- commande.py
+++ commande.py
@@ -1,49 +1,19 @@
 from datetime import datetime
 from enum import Enum
-from base import data
+
+from status import Status
 
 
 class Commande:
-    class Status(Enum):
-        En_cours = 'En cours'
-        Termine = 'Terminé'
-    
-    def __init__(self, id_commande, id_personnel, id_Panier, id_client, date, status=Status.En_cours):
-        self.id_commande = id_commande
+
+    def __init__(self, id_Panier, id_client, date, montant, status: Status, id_personnel=0):
+        # self.id_commande = id_commande
         self.id_personnel = id_personnel
         self.id_Panier = id_Panier
         self.id_client = id_client
         self.date = date
         self.status = status
-    
-    def update_status(self, new_status):
-        if new_status in Commande.Status:
-            self.status = new_status
-        else:
-            raise ValueError(f"{new_status} n'est pas un status valide")
-    
-    def __str__(self):
-        return f"Commande ID: {self.id_commande}, Status: {self.status.value}"
-
-
-    #def __str__(self):
-       # return (f" Votre Commande est passée avec : id_commande={self.id_commande} , id_personnel={self.id_personnel}, id_Panier={self.id_Panier} "
-           #     f"par le client: {self.id_client} au :{self.date.strftime('%Y-%m-%d')}, Le status de la commande est ={self.status.value}")
-        
+        self.montant = montant
 
     def __str__(self):
-        return f"Commande {self.id_commande} ({self.status.value})"
-
-# Création d'un objet de la classe Commande
-commande1 = Commande(
-    id_commande=1,
-    id_personnel=2,
-    id_Panier=3,
-    id_client=4,
-    date=datetime.now(),
-    status=Commande.Status.En_cours
-)
-data.liste_commande.append(commande1)
-print("voici la liste des commandes:")
-for commandes in data.liste_commande:
-    print(commandes)
+        return f"Commande {self.id_client}, {self.montant} ({self.status.value})"
